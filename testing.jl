e8e46c7b1e44606a06e6f8629514e061fc639006
MarcusGalea <s194336@student.dtu.dk>
2025-01-09 00:51:53 +0100

    Update testing.jl

--- testing.jl
+++ testing.jl
@@ -49,17 +49,25 @@ n,m = length(basis), size(ddprob)[1]
 constraints = [
     ξ[3] ~  10;
     ξ[2] + ξ[3] ~ 0
-]
-
+] 
+n_c = length(constraints)
 
 #### EXPERIMENTAL
+
+λ = 1e-5
+
 #evaluate basis at the data points
 Θ = basis(X,[],t)'
 C,d = linear_system(constraints, hcat(Ξ)) 
-
-A = vcat(hcat(blockdiags(Θ'Θ, length(u)),C'), hcat(C, zeros(size(C,1),size(C,1))))
+A = vcat(hcat(blockdiags(Θ'Θ, m),C'), hcat(C, zeros(n_c,n_c)))
 b = vcat(vec(Θ'*DX'),d)
-Θ_est= A\b
+ξ_est= (A\b)[1:end-n_c]
+
+#find elements close to zero in ξ_est
+
+idx = findall(x->abs(x)<λ, ξ_est)
+
+
 
 """
 create_block_diag_matrix function takes in a matrix and the number of copies and returns a block diagonal matrix with the given number of copies of the input matrix
@@ -109,211 +117,9 @@ C,d = linear_system(constraints, hcat(Ξ))
 C
 
 
-diagm
 
 opt = STLSQ(exp10.(-5:0.1:-1))
 ddsol = solve(ddprob, basis, opt, options = DataDrivenCommonOptions(digits = 1))
 println(get_basis(ddsol))
 println(get_parameter_map(get_basis(ddsol)))
 
-
-
-
-diag
-
-
-
-
-A = ddprob.X
-ddprob.DX
-DataDrivenSparse.init_cache(opt)
-
-
-struct STLSQ2{T <: Union{Number, AbstractVector}, R <: Number} <:
-    DataDrivenSparse.AbstractSparseRegressionAlgorithm
- """Sparsity threshold"""
- thresholds::T
- """Ridge regression parameter"""
- rho::R
-
- function STLSQ2(threshold::T = 1e-1, rho::R = zero(eltype(T))) where {T, R <: Number}
-     @assert all(threshold .> zero(eltype(threshold))) "Threshold must be positive definite"
-     @assert rho>=zero(R) "Ridge regression parameter must be positive definite!"
-     return new{T, R}(threshold, rho)
- end
-end
-
-Base.summary(::STLSQ2) = "STLSQ2"
-
-struct STLSQ2Cache{usenormal, C <: AbstractArray, A <: BitArray, AT, BT, ATT, BTT} <:
-    DataDrivenSparse.AbstractSparseRegressionCache
- X::C
- X_prev::C
- active_set::A
- proximal::SoftThreshold
- A::AT
- B::BT
- # Original Data
- Ã::ATT
- B̃::BTT
-end
-
-function init_cache(alg::STLSQ2, A::AbstractMatrix, b::AbstractVector)
- init_cache(alg, A, permutedims(b))
-end
-
-function init_cache(alg::STLSQ2, A::AbstractMatrix, B::AbstractMatrix)
- n_x, m_x = size(A)
- @assert size(B, 1)==1 "Caches only hold single targets!"
- @unpack rho = alg
- λ = minimum(get_thresholds(alg))
-
- proximal = get_proximal(alg)
-
- if n_x <= m_x && !iszero(rho)
-     X = A * A' + rho * I
-     Y = B * A'
-     usenormal = true
- else
-     usenormal = false
-     X = A
-     Y = B
- end
-
- coefficients = Y / X
-
- prev_coefficients = zero(coefficients)
-
- active_set = BitArray(undef, size(coefficients))
-
- active_set!(active_set, proximal, coefficients, λ)
-
- return STLSQ2Cache{usenormal, typeof(coefficients), typeof(active_set), typeof(X),
-     typeof(Y), typeof(A), typeof(B)}(coefficients, prev_coefficients,
-     active_set, get_proximal(alg),
-     X, Y, A, B)
-end
-
-function step!(cache::STLSQ2Cache, λ::T) where {T}
- @unpack X, X_prev, active_set, proximal = cache
-
- X_prev .= X
-
- step!(cache)
-
- proximal(X, active_set, λ)
- return
-end
-
-function step!(cache::STLSQ2Cache{true})
- @unpack X, A, B, active_set = cache
- p = vec(active_set)
- X[1:1, p] .= /(B[1:1, p], A[p, p])
- return
-end
-
-function step!(cache::STLSQ2Cache{false})
- @unpack X, A, B, active_set = cache
- p = vec(active_set)
- X[1:1, p] .= /(B, A[p, :])
- return
-end
-import CommonSolve
-
-function CommonSolve.solve!(ps::DataDrivenDiffEq.InternalDataDrivenProblem{
-                                                          <:DataDrivenSparse.AbstractSparseRegressionAlgorithm
-                                                          })
-    @unpack alg, basis, testdata, traindata, problem, options, transform = ps
-
-    results = map(traindata) do (X, Y)
-        __sparse_regression(ps, X, Y)
-    end
-
-    # Get the best result based on test error, if applicable else use testerror
-    sort!(results, by = l2error)
-
-    # Convert to basis
-    best_res = first(results)
-
-    # Transform the best coefficients
-    coefficients = permutedims(copy(get_coefficients(best_res)))
-    coefficients = permutedims(StatsBase.transform(transform, coefficients))
-    new_basis = DataDrivenDiffEq.__construct_basis(coefficients, basis, problem, options)
-
-    DataDrivenSolution(new_basis, problem, alg, results, ps, best_res.retcode)
-end
-
-function __sparse_regression(ps::InternalDataDrivenProblem{
-                                                           <:AbstractSparseRegressionAlgorithm
-                                                           }, X::AbstractArray,
-                             Y::AbstractArray)
-    @unpack alg, testdata, options, transform = ps
-
-    coefficients, optimal_thresholds, optimal_iterations = alg(X, Y, options = options)
-
-    trainerror = sum(abs2, Y .- coefficients * X)
-
-    X̃, Ỹ = testdata
-
-    if !isempty(X̃)
-        testerror = sum(abs2, Ỹ .- coefficients * X̃)
-    else
-        testerror = nothing
-    end
-
-    retcode = DDReturnCode(1)
-
-    dof = sum(abs.(coefficients) .> 0.0)
-
-    SparseRegressionResult(coefficients, dof, optimal_thresholds,
-                           optimal_iterations, testerror, trainerror,
-                           retcode)
-end
-
-function __sparse_regression(ps::InternalDataDrivenProblem{<:ImplicitOptimizer},
-                             X::AbstractArray, Y::AbstractArray)
-    @unpack alg, testdata, options, transform, basis, problem, implicit_idx = ps
-    @assert DataDrivenDiffEq.is_implicit(basis) "The provided `Basis` does not have implicit variables!"
-
-    candidate_matrix = zeros(Bool, size(implicit_idx))
-    idx = ones(Bool, size(candidate_matrix, 2))
-
-    for i in axes(candidate_matrix, 1), j in axes(candidate_matrix, 2)
-        idx .= true
-        idx[j] = false
-        # We want only equations which are either dependent on the variable or on no other
-        candidate_matrix[i, j] = implicit_idx[i, j] || sum(implicit_idx[i, idx]) == 0
-    end
-
-    opt_coefficients = zeros(eltype(problem), size(candidate_matrix, 2),
-                             size(candidate_matrix, 1))
-    opt_thresholds = []
-    opt_iterations = []
-
-    foreach(enumerate(eachcol(candidate_matrix))) do (i, idx)
-        # We enforce that one of the implicit variables is necessary for sucess
-        coeff, thresholds, iters = alg(X[idx, :], Y, options = options,
-                                       necessary_idx = implicit_idx[idx, i])
-        opt_coefficients[i:i, idx] .= coeff
-        push!(opt_thresholds, thresholds)
-        push!(opt_iterations, iters)
-    end
-
-    trainerror = sum(abs2, opt_coefficients * X)
-
-    X̃, Ỹ = testdata
-
-    if !isempty(X̃)
-        testerror = sum(abs2, opt_coefficients * X̃)
-    else
-        testerror = nothing
-    end
-
-    retcode = DDReturnCode(1)
-
-    dof = sum(abs.(opt_coefficients) .> 0.0)
-
-    SparseRegressionResult(opt_coefficients, dof, opt_thresholds,
-                           opt_iterations, testerror, trainerror,
-                           retcode)
-end

6cfaf8c1cef049c9e33f2708c22d03ed68287751
MarcusGalea <s194336@student.dtu.dk>
2025-05-06 10:39:11 +0200

    recent UDE changes

--- UDEs/Enzyme_UDE_testing.jl
+++ UDEs/Enzyme_UDE_testing.jl
@@ -80,13 +80,10 @@ end
 
 # Multilayer FeedForward
 U = Lux.Chain(
-    Lux.Dense(3,5,rbf), Lux.Dense(5,5, rbf), Lux.Dense(5,5, rbf), Lux.Dense(5,3)
+    Lux.Dense(3,5,rbf), Lux.Dense(5,5, rbf),Lux.Dense(5,5, rbf), Lux.Dense(5,3)
 )
 # Get the initial parameters and state variables of the model
 p_nn, st_nn = Lux.setup(rng, U)
-
-
-U([0.0;0.0;0.0], p_nn, st_nn) # test the model with a random input
 
 
 
@@ -97,13 +94,14 @@ u0map = Dict(:E => u0[1], :S => u0[2], :ES => u0[3])
 pmap = Dict(:k1 => p[1], :k2 => p[2])
 
 vars = species(known_rn)
-odesys = convert(ODESystem, rn)
+odesys = convert(ODESystem, known_rn)
 odesys = complete(structural_simplify(odesys)) # structural simplify the ODE system
-fun = ODEFunction(odesys, vars, parameters(rn)) # convert to ODEFunction
+fun = ODEFunction(odesys, vars, parameters(known_rn)) # convert to ODEFunction
 du0 = similar(u0)
 
 
-p_combined = (;ode = p, nn = p_nn) # combine the parameters into a single dictionary
+p_kass_guess = 0.1 #" initial guess for the association rate constant
+p_combined = (;ode = [p_kass_guess], nn = p_nn) # combine the parameters into a single dictionary
 p_combined = ComponentVector{Float64}(p_combined)
 function combined_model!(du, u, p, t)
     # Get the neural network output
@@ -114,14 +112,17 @@ function combined_model!(du, u, p, t)
     du .+= nn_out
 end
 
-prob_nn = ODEProblem(combined_model!,u0,tspan, p_combined, saveat = time)
-sol_nn = solve(prob_nn, Tsit5(), saveat = time,callback = cb, tstops = [diss_time])
+neural_model! = (du, u, p, t) -> first(U(u, p.nn, st_nn)) # neural network model
+
+neural_problem = ODEProblem(neural_model!, u0, tspan, p_nn, saveat = time)
+combined_problem = ODEProblem(combined_model!,u0,tspan, p_combined, saveat = time)
+sol_nn = solve(combined_problem, Tsit5(), saveat = time,callback = cb, tstops = [diss_time])
 plot(sol_nn, xlabel = "t", ylabel = "Concentration", title = "Enzyme Kinetics with Neural Network", label = ["E" "S" "ES"])
 
 
 
-function predict(θ, X = u0, T = time)
-    _prob = remake(prob_nn, u0 = X, tspan = (T[1], T[end]), p = θ)
+function predict(θ, X = u0, T = time; problem = combined_problem)
+    _prob = remake(problem, u0 = X, tspan = (T[1], T[end]), p = θ)
     Array(solve(_prob, Tsit5(), saveat = T,
                 abstol=1e-6, reltol=1e-6,
                 sensealg = ForwardDiffSensitivity(),
@@ -129,12 +130,10 @@ function predict(θ, X = u0, T = time)
                 ))
 end
 
-X̂ = predict(p_combined, u0, time) # test the model with a random input
-# Multiple shooting like loss
 
+λ = 1e-2 # regularization parameter
 
-λ = 1e-3 # regularization parameter
-
+# Loss function
 function loss(θ)
     # Start with a regularization on the network
     l = convert(eltype(θ), λ)*sum(abs2, θ[3:end]) ./ length(θ[3:end])
@@ -146,47 +145,104 @@ end
 
 # Container to track the losses
 
-losses = Float64[]
 
-function cb_nn(p, l)
+maxiters = 200
+skipping = 5 # skipping for plotting
+xlim = (0, maxiters)
+ylim = (1e-4, 1e+1)
+layout_setting_1 = @layout [a b; c]
+layout_setting_2 = @layout [a b]
+layout_setting_3 = @layout [a;b]
+
+
+function cb_nn(state, l; plot_every = 0, SINDyplot_every = 0, lossplot_every = 0)
   push!(losses, l)
   if length(losses)%50==0
       println("Current loss after $(length(losses)) iterations: $(losses[end])")
   end
+  pl_trajectory = nothing
+    pl_losses = nothing
+    p2 = nothing
+  if plot_every > 0 || SINDyplot_every > 0
+    X̂ = predict(state.u, u0, time)
+  end
+  if plot_every > 0 && (length(losses)-1)%plot_every == 0
+    pl_trajectory = scatter(time[1:skipping:end], signal[:,1:skipping:end]', labels = ["E" "S" "ES"].*" mesurements", markersize = 3, 
+    xlabel = "t", ylabel = "Concentration", title = "Enzyme Kinetics", legend = :topright)
+    plot!(pl_trajectory, data.t, transpose(X̂), label = "NN Approximation", markersize = 3)
+    # display(pl_trajectory)  
+  end
+  if SINDyplot_every > 0 && (length(losses)-1)%SINDyplot_every == 0
+    DX̂_NN= first(U(X̂, state.u.nn, st_nn))
+    rx_nn, paramsest_nn = ReactionSINDy(rs_no_ass, X̂ , DX̂_NN; threshold = threshold , lambda = lambda, verbose = false)
+    paramsest_nn[k_ass] = state.u[1]
+    p2 = visualize_reaction_network(rs_all_combs, rx_nn, paramsest_nn; params_true = params_true)
+    # display(p2)
+  end
+  if lossplot_every > 0 && (length(losses) -1)%lossplot_every == 0
+    i = length(losses)
+    if i < maxiters
+        pl_losses = plot(1:i, losses[1:i], yaxis = :log10, 
+        xlabel = "Iterations", ylabel = "Loss", label = "ADAM", color = :blue, xlim = xlim, ylim = ylim)
+    else
+        pl_losses = plot(1:maxiters, losses[1:maxiters], yaxis = :log10, xlabel = "Iterations", ylabel = "Loss", 
+        label = "ADAM", color = :blue)
+        plot!(pl_losses, maxiters+1:length(losses), losses[maxiters+1:end], yaxis = :log10, ylim = ylim,
+        xlabel = "Iterations", ylabel = "Loss", label = "BFGS", color = :red)
+    end
+    # display(pl_losses)
+    plot_combined = plot(pl_trajectory, p2, pl_losses, layout = layout_setting_1)
+    # display(plot_combined)
+    frame(animation, plot_combined)
+  end
   return false
 end
 
+animation = Plots.Animation()
+losses = Float64[]
+plot_every = 1
+
+modified_cb(args...) = cb_nn(args...; lossplot_every = plot_every, SINDyplot_every = plot_every, plot_every = plot_every)
 # First train with ADAM for better convergence -> move the parameters into a
 adtype = Optimization.AutoZygote()
 optf = Optimization.OptimizationFunction((x,p)->loss(x), adtype)
 optprob = Optimization.OptimizationProblem(optf, p_combined)
-res1 = Optimization.solve(optprob, PolyOpt(), callback=cb_nn, maxiters = 200)
+res1 = Optimization.solve(optprob, PolyOpt(), callback=modified_cb, maxiters = maxiters)
 p_trained = res1.minimizer # trained parameters
 
 # Plot the losses
 pl_losses = plot(1:200, losses[1:200], yaxis = :log10, xaxis = :log10, xlabel = "Iterations", ylabel = "Loss", label = "ADAM", color = :blue)
 plot!(201:length(losses), losses[201:end], yaxis = :log10, xaxis = :log10, xlabel = "Iterations", ylabel = "Loss", label = "BFGS", color = :red)
 # Rename the best candidate
-
+#save animation as gif
+# destination = "Enzyme_UDE.gif"
+gif(animation, destination, fps = 10)
 
 
 ## Analysis of the trained network
 # Plot the data and the approximation
 ts = first(data.t):mean(diff(data.t))/2:last(data.t)
-X̂ = predict(p_trained, u0, time)
-# Trained on noisy data vs real solution
-pl_trajectory = plot(data.t, transpose(X̂), ylabel = "t", xlabel ="x(t), y(t)", label = ["UDE Approximation" nothing])
+X̂ = predict(p_trained, u0, time) # test the model with a random input
+
+# # plot(data.t, DX̂_NN', label = "NN Approximation", markersize = 3, xlabel = "t", ylabel = "Concentration", title = "Enzyme Kinetics with Neural Network")
+# # Trained on noisy data vs real solution
+# plot(pl_trajectory ,data.t, transpose(X̂), ylabel = "Concentration", xlabel ="t", label = ["UDE Approximation" nothing])
+# #NEURAL NETWORK PREDICTION
+# plot!(pl_trajectory, data.t, transpose(X̂_NN), label = "NN Approximation", markersize = 3)
+# skip = 5
+
+
+
+
+# Network prediction
+# plot!(pl_trajectory, data.t, transpose(X̂_NN), label = "NN Approximation", markersize = 3)
+
 #scatter!(t, signal[1,:], color = :black, label = "Measurements")
 #SKIP BAD CODE
 # ymeasurements = unique!(vcat(signal...))
 # tmeasurements = unique!(vcat([[time[1], time[end]] for time in eachrow(time)]...))
 # scatter!(tmeasurements, ymeasurements, color = :black, label = nothing, legend = :topleft)
 ## TRY AGAIN
-skip = 5
-scatter!(pl_trajectory, time[1:skip:end], signal[:,1:skip:end]', labels = ["E" "S" "ES"].*" mesurements", legend = :topleft, markersize = 3)
-
-
-
 
 
 using Combinatorics
@@ -219,14 +275,14 @@ for lhs in combs
     end
 end
 reactions
-@named rs = ReactionSystem(reactions, t, vars, vcat(k...))
-ode_rs = convert(ODESystem, complete(rs))
+@named rs_all_combs = ReactionSystem(reactions, t, vars, vcat(k...))
+ode_rs_all_combs = convert(ODESystem, complete(rs_all_combs))
 
 ode_rn = convert(ODESystem, complete(rn)) # convert to ODEFunction
 
 
-rx = rs
-ode = ode_rs
+rx = rs_all_combs
+ode = ode_rs_all_combs
 #
 #du_approx = PhysicsInformedRegression.finite_diff(data.u, data.t)
 function ReactionSINDy(rx, u, du_approx; threshold = 1e-3, lambda =0.0, verbose = false)
@@ -271,7 +327,9 @@ end
 
 
 threshold = 1e-1 # Set a threshold for parameter pruning
-rx, paramsest = ReactionSINDy(rs, ensemble_data, ensemble_du; threshold = threshold , lambda = 0.0, verbose = true)
+lambda = 0.4 # Set a regularization parameter for the regression
+rx, paramsest = ReactionSINDy(rs_all_combs, ensemble_data, ensemble_du; threshold = threshold , lambda = lambda, verbose = true)
+# rx, paramsest = ReactionSINDy(rs_all_combs, data.u, du_actual; threshold = threshold , lambda = 0.0, verbose = true)
 
 #sa fdprev_rn = nothing
 # while prev_rn != rx
@@ -293,15 +351,29 @@ plot(data.t, hcat(du_actual...)', xlabel = "t", ylabel = "Concentration", title 
 
 #plot data and sindy
 plot(data.t, hcat(data_sindy.u...)', label = ["E" "S" "ES"].*" SINDy", markersize = 3)
-scatter!(time[1:skip:end], signal[:,1:skip:end]', labels = ["E" "S" "ES"].*" mesurements", legend = :topright, markersize = 3)
+scatter!(time[1:skipping:end], signal[:,1:skipping:end]', labels = ["E" "S" "ES"].*" mesurements", legend = :topright, markersize = 3)
 
-params_true = Dict([equations(rs)[13].rate => p[2], equations(rs)[18].rate => p[1]])
-p1 = visualize_reaction_network(rs, rx, paramsest; params_true = params_true)
+
+k_ass = equations(rs_all_combs)[18].rate
+k_diss= equations(rs_all_combs)[13].rate
+params_true = Dict([k_diss => p[2], k_ass => p[1]])
+p1 = visualize_reaction_network(rs_all_combs, rx, paramsest; params_true = params_true)
 display(p1)
 
 
+### SINDy ON NN
+###
+X̂ = predict(p_trained, u0, time)
+#Network prediction
+DX̂_NN= first(U(X̂, p_trained.nn, st_nn))
+rs_no_ass = remove_params(rs_all_combs, k_ass)
+rx_nn, paramsest_nn = ReactionSINDy(rs_no_ass, X̂ , DX̂_NN; threshold = threshold , lambda = lambda, verbose = true)
+paramsest_nn[k_ass] = p_trained.ode[1]
+
+p2 = visualize_reaction_network(rs_all_combs, rx_nn, paramsest_nn; params_true = params_true)
+
 ###3# visualization
-function visualize_reaction_network(rs, rx, paramsest; params_true = nothing, kwargs..., annotate = true)
+function visualize_reaction_network(rs, rx, paramsest; params_true = nothing, annotate = true, kwargs...)
     """
     Visualize the reaction network with parameters as circles.
 
@@ -318,7 +390,7 @@ function visualize_reaction_network(rs, rx, paramsest; params_true = nothing, kw
     gt_rate_maps = Dict()
     combs = vcat([[comb for comb in combinations(species(rs),i)] for i in 1:2]...)
     comb_strings_seperated = [Set(string.(comb)) for comb in combs]
-    for reaction in equations(rx)
+    for reaction in equations(rs)
         i = findfirst(isequal(Set(string.(reaction.substrates))), comb_strings_seperated)
         j = findfirst(isequal(Set(string.(reaction.products))), comb_strings_seperated)
         rate_maps[reaction.rate] = (i, j)
@@ -364,7 +436,8 @@ function visualize_reaction_network(rs, rx, paramsest; params_true = nothing, kw
         y_gt = [gt_rate_maps[param][1] for param in keys(params_true)] .* spacing
 
         sizes_gt = [abs(param) / max_param_value * max_radius * 100 for param in values(params_true)]  # Scale for visualization
-        scatter!(p1, x_gt, y_gt, m = (7, :white, stroke(1, color), sizes_gt), label="Ground Truth Parameters", legend=:topright, color = :transparent)
+        scatter!(p1, x_gt, y_gt, m = (7, :white, stroke(1, color), sizes_gt), label="Ground Truth Parameters", legend=:topright,) 
+        #color = :transparent)
     end
 
 
@@ -385,4 +458,60 @@ function visualize_reaction_network(rs, rx, paramsest; params_true = nothing, kw
     end
     return p1
 end
-# Display the plot
+
+
+
+
+
+function remove_params(rn, param_to_remove)
+    """
+    Remove parameter from the ODE system and return the new system.
+
+    Parameters:
+    - `rn`: The original Catalyst Reaction System.
+    - `param_to_remove`: A parameter to be removed.
+
+    Returns:
+    - `new_rn`: The new Reaction System with parameters removed.
+    """
+    # Filter reactions where the rate is not in `param_to_remove`
+    new_reactions = [reaction for reaction in equations(rn) if !isequal(reaction.rate, param_to_remove)]
+
+    # Filter parameters that are not in `param_to_remove`
+    new_params = [param for param in parameters(rn) if !isequal(param, param_to_remove)]
+
+    # Create a new Reaction System with the updated reactions and parameters
+    @named new_rn = ReactionSystem(new_reactions, rn.t, species(rn), new_params)
+end
+
+
+
+######## DOESN*T MATTER ###########
+
+function insert_known_params(ode_rn, known_params)
+    """
+    insert known parameters from the ODE system and return the new system.
+
+    Parameters:
+    - `ode_rn`: The original ODE system.
+    - `known_params`: A dictionary of known parameters to be inserted.
+
+    Returns:
+    - `new_ode`: The new ODE system with known parameters inserted.
+    """
+    # Get the equations and parameters of the ODE system
+    new_eqs = Equation[]
+
+    new_eqs = Equation[]
+    for eq in equations(ode_rn)
+        new_eq = substitute(eq, known_params)
+        new_eqs = push!(new_eqs, new_eq)  
+    end
+    #remove parameters that are in known_params
+    new_params = [param for param in parameters(ode_rn) if !(param in keys(known_params))]
+    @named new_ode = ODESystem(new_eqs, t, ModelingToolkit.get_unknowns(ode_rn), new_params)
+    return new_ode
+end
+
+
+
